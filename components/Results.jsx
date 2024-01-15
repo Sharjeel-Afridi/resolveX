6143d22ab60f93c4bb30195a5415f89b3fb56e47
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-01-16 01:56:05 +0530

    modified code

--- components/Results.jsx
+++ components/Results.jsx
@@ -6,26 +6,24 @@ import { useEffect, useState } from "react";
 const Results = ({api, input}) => {
 
     const [selectedFilter, setSelectedFilter] = useState('All');
-
-    useEffect(()=>{
-        document.getElementById('new-div').scrollIntoView({behavior:'smooth'});
-    },[])
-
-
-    console.log("results 1");
-   
     const results = useSearch(api,input);
     
-    let newResults = [];
+    useEffect(()=>{
+        document.getElementById('new-div').scrollIntoView({behavior:'smooth'});
+    }, []);
     
-    
+   
+    // filled with dummy to make its size != 0
+    let newResults = ["dummy"]; 
     if(selectedFilter != "All"){
         newResults = results.filter(element => element.item.year === selectedFilter);
     }
-    let showTotal= false;
-    if(results.length != 0){
-        showTotal = true;
-    }
+   
+    //checks if there are results to show
+    const showTotal = results.length !== 0;
+
+    //checks whether filtered array is empty or there are no results
+    const noResults = newResults.length === 0 || !showTotal;
 
     function handleNotesClick(link){
         window.open(link,'_blank');
@@ -34,40 +32,42 @@ const Results = ({api, input}) => {
 
     const handleFilterChange = (filter) => {
         setSelectedFilter(filter);
-        console.log(filter)
         
     };
 
-
+    //use filtered array if filter is selected
+    const resultType = selectedFilter != "All" ? newResults : results; 
+    
     return (
         <div className="new-div-container">
-            {showTotal && <div className="total-div">
-                <span className="total-results">Total Results: {selectedFilter != "All" ? newResults.length : results.length}</span> 
-                <div className="filter-dropdown">
-                    <label>Year</label>
-                    <select value={selectedFilter} onChange={(e) => handleFilterChange(e.target.value)}>
-                        <option value="All">All</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
-                </div>
-            </div>}
+            {showTotal && (
+                <div className="total-div">
+                    <span className="total-results">Total Results: {selectedFilter != "All" ? newResults.length : results.length}</span> 
+                    <div className="filter-dropdown">
+                        <label>Year:</label>
+                        <select value={selectedFilter} onChange={(e) => handleFilterChange(e.target.value)}>
+                            <option value="All">All</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </div>
+                </div>)
+            }
             
             <div className="new-div" id="new-div">
-                {selectedFilter != "All" ? newResults.map((element) => (
-                    <div key={element.item.id} className="notes-div" onClick={() => handleNotesClick(element.item.notes)} >
-                    <span className="title">{element.item.Title}</span>
-                    <span className="year">Year: {element.item.year}</span>
-                    </div>
-                )) : 
-                results.map((element) => (
-                    <div key={element.item.id} className="notes-div" onClick={() => handleNotesClick(element.item.notes)} >
-                    <span className="title">{element.item.Title}</span>
-                    <span className="year">Year: {element.item.year}</span>
-                    </div>
-                ))}
+                {noResults ? (
+                    showTotal && <h1 className="no-results">No Results Found</h1>
+                    ) : (
+                        resultType.map((element) => (
+                            <div key={element.item.id} className="notes-div" onClick={() => handleNotesClick(element.item.notes)} >
+                                <span className="title">{element.item.Title}</span>
+                                <span className="year">Year: {element.item.year}</span>
+                            </div>
+                        ))
+                    )
+                }
             </div>
             
            
