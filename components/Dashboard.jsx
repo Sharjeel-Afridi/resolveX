05c576c2a250ae02fe315a15e9d2ac524ad6fa83
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-02-28 14:24:59 +0530

    modified css

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 import useLogout from "../utils/useLogout";
 import { useContext, useEffect } from "react";
 import UserContext from "../utils/UserContext";
+import Sun from "../sun.png";
+import Moon from "../moon.png";
 import useVerified from "../utils/useVerified";
 import DarkModeContext from "../utils/DarkModeContext";
 
 const Dashboard = () => {
     const logout = useLogout();
-    const { isDarkMode } = useContext(DarkModeContext);
+    const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
     const { loggedinUser, setLoggedinUser } = useContext(UserContext);
 
     useEffect(() =>{
@@ -23,8 +25,10 @@ const Dashboard = () => {
         <div>
             <nav>
                 <Link to="/" className={`route-btn ${isDarkMode ? 'dark-mode' : ''}`}>resolveX</Link>
-                  
-                <h3 className="username">{loggedinUser}</h3>
+                <div className="nav-right">
+                    <button className="toggle-btn" id="toggle-btn" onClick={toggleDarkMode}><img src={isDarkMode ? Sun : Moon}/> </button>
+                    <h3 className="username">{loggedinUser}</h3>
+                </div>
                     
             </nav>
             <div className="main-dashboard">
